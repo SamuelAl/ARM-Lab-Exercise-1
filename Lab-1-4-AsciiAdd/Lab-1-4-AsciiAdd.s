b696d57d1d7ca4c5331f31c385c045638220094a
einstenz <33717014+einstenz@users.noreply.github.com>
2019-09-26 19:10:29 +0100

    Finished ASCII add exercised
    - Answer tested

--- Lab-1-4-AsciiAdd/Lab-1-4-AsciiAdd.s
+++ Lab-1-4-AsciiAdd/Lab-1-4-AsciiAdd.s
@@ -9,7 +9,10 @@
 	LDR	R1, ='2'	; Load R1 with ASCII code for symbol '2'
 	LDR	R2, ='4'	; Load R2 with ASCII code for symbol '4'
 
-	; your program goes here
+	SUB R1, R1, #0x30 	; Convert values to number form
+	SUB R2, R2, #0x30
+	ADD R0, R1, R2		; Add values 
+	ADD R0, R0, #0x30	; Convert back to ASCII
 
 STOP	B	STOP
 
