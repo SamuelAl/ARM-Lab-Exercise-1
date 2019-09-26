af3ae80ab084013724c27c448ebc4d3e95f0db8b
einstenz <33717014+einstenz@users.noreply.github.com>
2019-09-26 19:30:10 +0100

    Finished Lab 1-5
    Answer Verified

--- Lab-1-5-AsciiValue/Lab-1-5-AsciiValue.s
+++ Lab-1-5-AsciiValue/Lab-1-5-AsciiValue.s
@@ -11,7 +11,25 @@
 	LDR	R3, ='3'	; Load R3 with ASCII code for symbol '3'
 	LDR	R4, ='4'	; Load R4 with ASCII code for symbol '4'
 
-	; your program goes here
+	SUB R1, R1, #0x30 	; Convert values to number form
+	SUB R2, R2, #0x30
+	SUB R3, R3, #0x30
+	SUB R4, R4, #0x30
+	
+	MOV R0, R4 
+	
+	LDR	R5, =1000
+	MUL R1, R5, R1 		; x10 position
+	ADD R0, R1, R0
+	
+	LDR R5, =100
+	MUL R2, R5, R2		; x100 position
+	ADD R0, R2, R0
+	
+	LDR R5, =10
+	MUL	R3, R5, R3		; x1000 position
+	ADD R0, R3, R0
+	
 
 STOP	B	STOP
 
